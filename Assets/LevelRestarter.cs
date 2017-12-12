28c27a91b37f5a3e853308f7629a38edecbe93d0
Wouter Vanmulken <woutervanmulken@msn.com>
2017-12-12 15:43:21 +0100

    final changes and removed animator

---
+++ Assets/LevelRestarter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestarter : MonoBehaviour {
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+	}
+}
