7cbe1b6aabc16ef202e248c2471a30e7caa82c95
Teun Willems <teun.willems@live.nl>
2017-12-12 11:34:03 +0100

    Added confetti

---
+++ Assets/ParticleTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleTrigger : MonoBehaviour {
+
+	public ParticleSystem ParticleSystem { get; private set; }
+	
+	// Use this for initialization
+	public void Start ()
+	{
+	}
+
+	public void TriggerParticles()
+	{
+		if (this.ParticleSystem == null)
+		{
+			this.ParticleSystem = GetComponent<ParticleSystem>();
+		}
+		this.ParticleSystem.Play();
+	}
+}
