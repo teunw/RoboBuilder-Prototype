810e8d430ae4ff45fae89a094f40d27a72680b5e
Wouter Vanmulken <woutervanmulken@msn.com>
2017-12-12 13:09:56 +0100

    Added objectivescript

---
+++ Assets/RobotObjective.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class RobotObjective : MonoBehaviour
+{
+	public RobotBehaviourScript RobotBehaviourScript { get; private set; }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.HasComponent<RobotBehaviourScript>())
+		{
+			this.RobotBehaviourScript = other.gameObject.GetComponent<RobotBehaviourScript>();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.HasComponent<RobotBehaviourScript>() && 
+		    RobotBehaviourScript != null &&
+		    other.gameObject.GetComponent<RobotBehaviourScript>().Equals(this.RobotBehaviourScript))
+		{
+			this.RobotBehaviourScript = null;
+		}
+	} 
+}
